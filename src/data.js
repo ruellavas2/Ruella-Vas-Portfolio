6e92a449bb1975e67f22e9858c839b8f7342ab2d
Ruella Vas <ruellavas@R7HP7225PW.local>
2023-04-24 00:43:10 +0100

    Deploy

--- src/data.js
+++ src/data.js
@@ -3,50 +3,50 @@ export const projects = [
     title: "DIABERRY",
     subtitle: "Android Mobile app",
     description:
-      "This is a Dibates mobile App that will help you track you diabetes",
+      "This is a Dibates mobile App that will help you track you diabetes which was part of my final year project in my course for Computer Science.",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/ruellavas2/Android-Diabetes-App",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Scottish Widows Application",
+    subtitle: "React.js, Node.js",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Worked in a Scrum Team of 8 people on Lloyds Banking Group as Junior Developer to help Lloyds Advisers to tranfer client pension into different funds.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.scottishwidows.co.uk/online-services/mobile-app.html",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "MY COMMUNITY FINANCE",
+    subtitle: "React",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    "Built loan pages using React.js, Angular, and Salesforce while also testing adaptive UIs against all screen types. They mention being the only React developer responsible for building all the feature work for the Accept Loan Offer page and working closely with the Head of Engineering. The author also worked in a small scrum team and collaborated with an offshore team. Within two weeks of joining, they delivered a proof of concept (POC) to the CTO, Head of Engineering, Product Manager, and other stakeholders.",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "CERTIFICATIONS AND COURSES",
+    subtitle: "All my courses and learnings",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "Professional Scrum Master I (PSM1)- Issued by Scrum.org 11/2022, React.js and React Hooks- Issued by LinkedIn 2022,Essential training Jenkins, Express, Ui/UX- Issued by LinkedIn 2022",
+    image: "https://media3.giphy.com/media/cQ23bDqzbWbh240xQq/giphy.gif",
+    link: "https://www.linkedin.com/in/ruella-vas-0297/details/certifications/",
   },
 ];
 
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+      "Having worked with Ruella on 2 projects directly during our time at AND Digital, I am impressed by Ruella's passion and excellent skills. She is keen to take on tasks by herself and will proactively reach out to other teammates when she runs into issues or has questions. This has provided transparency to the whole team, as well as fostered the team culture of pair working and reaching out. Ruella is also an enthusiastic learner who will equip herself with new skills via LinkedIn Learning and other resources. Recently, she was certified as Professional Scrum Master by Scrum.org, which is a great addition on top of her practical experience working in Agile and Scrum team setup. Ruella is a quick learner and self starter who, I can imagine, will excel in any work place and bring innovation to the business",
+    image: "https://media.licdn.com/dms/image/C4E03AQE2pRZqt_OxCA/profile-displayphoto-shrink_100_100/0/1649102107794?e=1687996800&v=beta&t=3kDl7U1VOLP1Bq1XtH1psydNKRF5EEmnUNZ-J1y1UAo",
+    name: "Chloe Cheng",
+    company: "AND Digital",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
+      "I've worked with Ruella as her Scrum Master and coach and was absolutely delighted to see her learn and grow rapidly in a highly complex and not necessarily junior-friendly environment. She's done incredibly well with finding her feet in a completely new industry and learning about the business, the technology and testing in place as well as Agile methods like Scrum. Ruella is a curious and resourceful learner and within a short time she managed to pick up work independently. She has a great can-do attitude and didn't hesitate to volunteer for public speaking opportunities to teach young people about tech, nor was she afraid about presenting her work to stakeholders. I believe she has a bright future ahead of her and I would love to work with her again.",
+    image: "https://media.licdn.com/dms/image/C4E03AQHQoJZon3gr9Q/profile-displayphoto-shrink_100_100/0/1643740539352?e=1687392000&v=beta&t=lp7-GAbRgketqykL0_TDKsdGmYdIGbdWGEqp_Zlaut0",
     name: "Ruben Alvarez",
-    company: "Dooble.io",
+    company: "AND Digital",
   },
 ];
 
